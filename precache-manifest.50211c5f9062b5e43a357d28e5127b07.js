ae5d976d24c7dceefdc5d7ee06f45c6bdbc89f51
TIANQI-PC\tqwan <twang2@ualberta.ca>
2018-12-30 22:39:45 -0700

    Updates

--- precache-manifest.50211c5f9062b5e43a357d28e5127b07.js
+++ precache-manifest.50211c5f9062b5e43a357d28e5127b07.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/Chat/static/js/runtime~main.e86f7a15.js"
   },
   {
-    "revision": "8e0cc616fa43caaeb58f",
-    "url": "/Chat/static/js/main.8e0cc616.chunk.js"
+    "revision": "5c92a304787d8f9afe43",
+    "url": "/Chat/static/js/main.5c92a304.chunk.js"
   },
   {
     "revision": "9522442f9534f9cb95a8",
     "url": "/Chat/static/js/1.9522442f.chunk.js"
   },
   {
-    "revision": "8e0cc616fa43caaeb58f",
+    "revision": "5c92a304787d8f9afe43",
     "url": "/Chat/static/css/main.87662ff3.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/Chat/static/css/1.6f9d5941.chunk.css"
   },
   {
-    "revision": "6975fb4b3d74abbbae892ac165807d2e",
+    "revision": "2c49b0657ba0bbcbe9fd106afc43c471",
     "url": "/Chat/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Chat/precache-manifest.7244846e1c7033205fcb0834ce112c17.js"
+  "/Chat/precache-manifest.50211c5f9062b5e43a357d28e5127b07.js"
 );
 
 workbox.clientsClaim();
