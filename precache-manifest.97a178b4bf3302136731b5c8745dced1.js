af0b8dc271d01fa9d04953de7a12117888adc415
TIANQI-PC\tqwan <twang2@ualberta.ca>
2018-12-30 21:26:05 -0700

    Updates

---
+++ precache-manifest.97a178b4bf3302136731b5c8745dced1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e86f7a155166b07bff93",
+    "url": "/Chat/static/js/runtime~main.e86f7a15.js"
+  },
+  {
+    "revision": "833c3506758a5f52672a",
+    "url": "/Chat/static/js/main.833c3506.chunk.js"
+  },
+  {
+    "revision": "9522442f9534f9cb95a8",
+    "url": "/Chat/static/js/1.9522442f.chunk.js"
+  },
+  {
+    "revision": "833c3506758a5f52672a",
+    "url": "/Chat/static/css/main.87662ff3.chunk.css"
+  },
+  {
+    "revision": "9522442f9534f9cb95a8",
+    "url": "/Chat/static/css/1.6f9d5941.chunk.css"
+  },
+  {
+    "revision": "e81031ba939b2df8df6c570d7452c720",
+    "url": "/Chat/index.html"
+  }
+];
